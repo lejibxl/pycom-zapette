70d890edd19b3b572499b5267e52f2ea0f2bf8df
lejibxl <57529300+lejibxl@users.noreply.github.com>
2020-02-03 14:47:29 +0100

    a

--- zapette.py
+++ zapette.py
@@ -5,10 +5,13 @@ from network import LoRa
 import socket
 import ubinascii
 import struct
-
+import logging.logging as logging
+from zapette.zapette_handler import ZapetteHandler
+l = logging.getLogger(__name__)
 class Zapette:
     def __init__(self, device_id, handler_rx=None, handler_tx=None ):
-        self._log("loraBlk init ...")
+        l.addHandler(ZapetteHandler("self"),2)
+        l.debug("loraBlk init ...")
         self.device_id = bytes([device_id])
         self._user_handler_rx  = handler_rx
         self._user_handler_tx  = handler_tx
@@ -41,11 +44,11 @@ class Zapette:
         global RX,TX,lpp
         events = lora.events()
         stats = lora.stats()
-        self._log("event: {}".format(events))
-        self._log("stats: {}".format(stats))
+        l.debug("event: {}".format(events))
+        l.debug("stats: {}".format(stats))
         if events & LoRa.RX_PACKET_EVENT:
             rx_frame = self.lora_sock.recv(256)
-            self._log("rx : {}".format(ubinascii.hexlify(rx_frame)))
+            l.debug("rx : {}".format(ubinascii.hexlify(rx_frame)))
             #header
             device_from = rx_frame[0:1]
             device_to = rx_frame[1:2]
@@ -54,19 +57,19 @@ class Zapette:
             payload = rx_frame[5:-1]
             crc = rx_frame[-1]
 
-            self._log("device_from:{} ,device_to:{} ,frame_type:{}, frame_cnt:{}, crc:{}, payload:{}".format(device_from,device_to,frame_type,frame_cnt,crc,payload))
+            l.debug("device_from:{} ,device_to:{} ,frame_type:{}, frame_cnt:{}, crc:{}, payload:{}".format(device_from,device_to,frame_type,frame_cnt,crc,payload))
             if device_to == self.device_id :
                 if frame_type ==  b'\x00' : #RX
-                    self._log("FRAME RX")
+                    l.debug("FRAME RX")
                     self._send_frame_ACK(device_from, frame_cnt, stats, b'\x01')
                     if self._user_handler_rx is not None:
                         lpp_rx=self.lpp.decode(payload)
                         if lpp_rx != None:
                             self._user_handler_rx(lpp_rx,{'rssi':stats.rssi,'snr':stats.snr})
                     else:
-                        self._log("no Handler")
+                        l.debug("no Handler")
                 elif frame_type ==  b'\x01' : #ACK
-                    self._log("FRAME ACK")
+                    l.debug("FRAME ACK")
                     #tmp = struct.unpack("bhf",payload) Bug
                     status =  payload[0]
                     rssi = struct.unpack("h",payload[1:3])[0]
@@ -76,12 +79,12 @@ class Zapette:
                     if self._user_handler_rx is not None:
                         self._user_handler_rx(None,{'rssi':stats.rssi,'snr':stats.snr})
             else:
-                self._log("messages for: {}".format(device_to))
+                l.debug("messages for: {}".format(device_to))
 
         elif events & LoRa.TX_PACKET_EVENT:
-            self._log("FRAME send")
+            l.debug("FRAME send")
         else:
-            self._log("???")
+            l.debug("???")
 
     def _send_frame_ACK(self, device_to, frame_cnt, stats, status):
         device_from = self.device_id
@@ -93,7 +96,7 @@ class Zapette:
         payload = status + struct.pack("h",rssi) + struct.pack("f",snr)
         crc = b'\x00'
         tx_frame = self.device_id + device_to + frame_type + frame_cnt + payload + crc
-        self._log("tx : {}".format(ubinascii.hexlify(tx_frame)))
+        l.debug("tx : {}".format(ubinascii.hexlify(tx_frame)))
         self.lora_sock.send(tx_frame)
 
     def send_frame_TX(self, device_to,msg):
@@ -105,7 +108,7 @@ class Zapette:
         crc = b'\x00'
         msg.reset()
         tx_frame = self.device_id + device_to + frame_type + frame_cnt + payload + crc
-        self._log("tx : {}".format(ubinascii.hexlify(tx_frame)))
+        l.debug("tx : {}".format(ubinascii.hexlify(tx_frame)))
         self.frame_ACK = None
         for i in range(0,self.tx_retries):
             self.lora_sock.send(tx_frame)
@@ -115,20 +118,11 @@ class Zapette:
                 self._user_handler_tx(i)
             while (utime.ticks_diff(deadline, utime.ticks_ms()) * ticks_sign) < 0:
                 if self.frame_ACK == frame_cnt:
-                    self._log("tx OK")
+                    l.debug("tx OK")
                     return self.frame_ACK_stats
-            self._log("tx NOK {}/{}".format(i+1,self.tx_retries))
+            l.debug("tx NOK {}/{}".format(i+1,self.tx_retries))
         return None
 
-    def _log(self, message, *args):
-        """
-        Outputs a log message to stdout.
-        """
-
-        print('[{:>10.3f}] {}'.format(
-            utime.ticks_ms() / 1000,
-            str(message).format(*args)
-            ))
 def test_rx(lpp_rx,stats):
     print(stats)
     if lpp_rx != None:
---
+++ zapette_handler.py
@@ -0,0 +1,31 @@
+
+from logging.logging import LoggingHandler
+from zapette.zapette import send_frame_TX
+
+class ZapetteHandler(LoggingHandler):
+
+    def __init__(self, zapette):
+        """Create an instance.
+
+        :param zapette: the lora trap  to which to write messages
+
+        """
+        self._zapette = zapette
+
+    def format(self, level, msg):
+        """Generate a string to log.
+
+        :param level: The level at which to log
+        :param msg: The core message
+
+        """
+        return super().format(level, msg) + '\r\n'
+
+    def emit(self, level, msg):
+        """Generate the message and write it to the UART.
+
+        :param level: The level at which to log
+        :param msg: The core message
+
+        """
+        send_frame_TX(self.format(level, msg))
